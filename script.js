ea9e1b58f956e9f7b1221bff877f90e20a6a3d59
lifeconsciousness <lifeconsciousness321@gmail.com>
2022-12-11 23:15:19 +0100

    objects w id, short, long links save localstorage

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const linkShortener = function(){
     const linkValidMessage = document.querySelector(".linkValid")
     const resultLink = document.querySelectorAll(".result-link")
     const previousLink = document.querySelectorAll(".prev-link")
+    const deleteIcons = document.querySelectorAll(".trashBin")
 
 
     //checks whether link is valid and outputs the result
@@ -19,8 +20,8 @@ const linkShortener = function(){
 
     //on button click executes shortening function
     shortenBtn.addEventListener("click", function(){
-        //shortenLink()
-        shortenLinkAsync()
+        shortenLink()
+        //shortenLinkAsync()
         message.innerText = "Click on the link to copy it "
     })
     
@@ -42,9 +43,9 @@ const linkShortener = function(){
                     etc: 'etc'
                 }) */
             })
-                .then(res => res.json())                                   //first convert the data from json string to js object
-                .then(data => displayLink(data.result.full_short_link))    //then pass the received data to the function 
-                .catch(error => console.log(error))                        //catch any errors
+                .then(res => res.json())                                        //first convert the data from json string to js object
+                .then(data => createLink(data.result.full_short_link, link))          //then pass the received data to the function 
+                .catch(error => console.log(error))                             //catch any errors
         } else{
             return
         }
@@ -60,7 +61,7 @@ const linkShortener = function(){
                 let response = await fetch(`https://api.shrtco.de/v2/shorten?url=${link}`)
                 if(response.ok){
                     const data = await response.json()
-                    displayLink(data.result.full_short_link)
+                    createLink(data.result.full_short_link)
                 }    
             } catch (error){
                 console.log("Error:" + error)
@@ -73,11 +74,56 @@ const linkShortener = function(){
 
 
 
-    function displayLink(shortenedLink){
-        resultLink.innerText = shortenedLink
+
+
+    const INDEX_LOCAL_STORAGE_KEY = 'indexKey'
+    const HISTORY_OBJECT_LOCAL_STORAGE_KEY = 'historyObjectKey'
+
+    let historyElementIndex = JSON.parse(localStorage.getItem(INDEX_LOCAL_STORAGE_KEY)) || 0
+    let historyElementsArray = JSON.parse(localStorage.getItem(HISTORY_OBJECT_LOCAL_STORAGE_KEY)) || []
+
+    function createLink(shortenedLink, firstLink){
+        resultLink[0].innerText = shortenedLink
+        previousLink[0].innerText = firstLink
+
+        let historyObject =  createLinkHistoryObject(shortenedLink, firstLink)
+        historyElementsArray.push(historyObject)
+
+        historyElementIndex++
+        console.log(historyElementsArray)
+        save()
+        render()
     }
 
-    
+    function createLinkHistoryObject(shortenedLink, firstLink){
+        return {
+            id: historyElementIndex,
+            shortLink: shortenedLink,
+            longLink: firstLink
+        }
+    }
+
+
+
+    function save(){
+        localStorage.setItem(INDEX_LOCAL_STORAGE_KEY, historyElementIndex)
+        localStorage.setItem(HISTORY_OBJECT_LOCAL_STORAGE_KEY, JSON.stringify(historyElementsArray))
+    }
+    function render(){
+        
+    }
+
+
+
+    deleteIcons.forEach(icon => addEventListener("click", deleteElement))
+
+    deleteIcons[1].dataset.index = 3
+
+    function deleteElement(event){
+        //console.log("delete " + event.target.dataset.index)
+    }
+
+
 
 
 
